43f5267a62c1375bc32eb5e163e50a957b984478
zulfanhanani <hanani.zulfan@gmail.com>
2026-04-21 10:51:23 +0700

    Update input method and modify index handling
    Refactor input handling and index validation logic.

--- soal1.c
+++ soal1.c
@@ -15,80 +15,48 @@
     char array[30];
     int idx;
 
-    scanf("%s", array);
+    fgets(array, sizeof(array), stdin);
     idx = strlen(array);
-    // printf("%d", idx);
-    // for (int i = 0; i < 10; i++){
-    //     scanf("%c", &array[i]);
-    //     if (array[i] == '\n'){
-    //         array[i] = '\0';
-    //         idx = i-1;
-    //         break;
-    //     }
-    // }
 
     char temparray[idx];
     int index[idx];
     for (int i = 0; i < idx; i++){
-        index[i] = -1;
+        index[i] = 1;
         temparray[i] = array[i];
     }
 
     //pengecekan
     for (int i = 0; i < idx; i++){
-        // int valid = 1;
         if (temparray[i] == '('){
-            // valid = 0;
             for (int j = i; j < idx; j++){
                 if (temparray[j] == ')'){
                     temparray[i] = 1;
                     temparray[j] = 1;
-                    // valid = 1;
                     break;
                 } 
             }
         }else if (temparray[i] == ')'){
-            // valid = 0;
             for (int j = 0; j < i; j++){
                 if (temparray[j] == '('){
                     temparray[i] = 1;
                     temparray[j] = 1;
-                    // valid = 1;
                     break;
                 }
             }
         }
-        // if (valid == 0){
-        //     index[i] = 1;
-        // }
         
     }
-    // printf("%s", temparray); //debug
     
     //pengecekan indeks tidak valid
     for (int i = 0; i < idx; i++){
         if (temparray[i] == '(' || temparray[i] == ')'){
-            index[i] = 1;
+            index[i] = -1;
         }
         // printf("%d ", index[i]); // debug
     }
 
-    //penghapusan karakter tidak valid
-    // for (int i = 0; i < idx; i++){
-    //     if (index[i] == 1){
-    //         if (i == idx - 1){
-    //             idx--;
-    //             // printf("\nBerhasil");
-    //         }else{
-    //             for (int j = i; j < idx - 1; j++){
-    //                 array[j] = array[j+1];
-    //             }
-    //             idx--;
-    //         }
-    //     }
-    // }
     for (int i = 0; i < idx; i++){
-        if (index[i] == -1){
+        if (index[i] == 1){
             printf("%c", array[i]);
         }
     }
