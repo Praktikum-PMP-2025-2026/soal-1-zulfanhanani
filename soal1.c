41a444595908ebe8d6ef8f3fe852cec9477e7f3e
zulfanhanani <hanani.zulfan@gmail.com>
2026-04-21 10:26:07 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -13,7 +13,7 @@
  int main() {
     char array[30];
     int idx;
-    
+
     for (int i = 0; i < 10; i++){
         scanf("%c", &array[i]);
         if (array[i] == '\n'){
@@ -29,34 +29,65 @@
         index[i] = -1;
         temparray[i] = array[i];
     }
-    
+
+    //pengecekan
     for (int i = 0; i < idx; i++){
+        int valid = 1;
         if (temparray[i] == '('){
+            valid = 0;
             for (int j = i; j < idx; j++){
                 if (temparray[j] == ')'){
                     temparray[i] = 1;
                     temparray[j] = 1;
+                    valid = 1;
+                    break;
+                } 
+            }
+        }else if (temparray[i] == ')'){
+            valid = 0;
+            for (int j = 0; j < i; j++){
+                if (temparray[j] == '('){
+                    temparray[i] = 1;
+                    temparray[j] = 1;
+                    valid = 1;
                     break;
                 }
             }
         }
+        // if (valid == 0){
+        //     index[i] = 1;
+        // }
+        
     }
-
+    // printf("%s", temparray); //debug
+    
+    //pengecekan indeks tidak valid
     for (int i = 0; i < idx; i++){
         if (temparray[i] == '(' || temparray[i] == ')'){
-            index[i] = i;
+            index[i] = 1;
         }
+        printf("%d ", index[i]); // debug
     }
 
+    //penghapusan karakter tidak valid
     for (int i = 0; i < idx; i++){
-        if (index[i] > 0){
-            for (int j = i; j < idx; j++){
-                array[j] = array[j+1];
+        if (index[i] == 1){
+            if (i == idx - 1){
+                idx--;
+                // printf("\nBerhasil");
+            }else{
+                for (int j = i; j < idx - 1; j++){
+                    array[j] = array[j+1];
+                }
+                idx--;
             }
         }
     }
 
-    printf("%s\n", array);
+    printf("\n%d Hasil: ", idx);
+    for (int i = 0; i < idx; i++){
+        printf("%c", array[i]);
+    }
 
     return 0;
  }
